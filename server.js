6ef4073d6a2317659fccd0f27a656937263201e1
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-01-11 15:05:37 -0500

    1

--- server.js
+++ server.js
@@ -14,28 +14,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-// MongoDB connection string
-// const uri =
-//     'mongodb+srv://ksltho:LGZmTn18FQl31C26@cluster0.5u1whyy.mongodb.net/?retryWrites=true&w=majority';
-
-// mongoose
-//     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB Connected'))
-//     .catch((err) => console.log(err));
-
-// Define routes
-// Get all events
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
+app.post('/api/v1/users', async (req, res) => {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+});
 
 try {
-    await mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URL);
     app.listen(port, () => {
         console.log(`server is listening at port ${port}`);
     });
